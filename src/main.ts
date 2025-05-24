8da46e03b105aff7490bafb79b88a13be3db2198
Sam Oen <samoen@gmail.com>
2025-05-24 12:41:36 +0800

    units

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './style.css'
+import bowman from './assets/units/bowman.png'
+import general from './assets/units/general.png'
+import plain from './assets/landscapes/landscape-plain.webp'
 
 document.querySelector<HTMLDivElement>('#loading')!.remove()
 
@@ -41,5 +44,106 @@ visual.style.height = 'max-content'
 visual.style.minHeight = '100%'
 wrapGameField.appendChild(visual)
 
+let imageBackground = document.createElement('div')
+imageBackground.style.position = 'absolute'
+imageBackground.style.top = '0'
+imageBackground.style.overflow = 'hidden'
+imageBackground.style.height = '100%'
+imageBackground.style.width = '100%'
+visual.appendChild(imageBackground)
+
+let bgAndGrad = document.createElement('div')
+bgAndGrad.style.position = 'relative'
+imageBackground.appendChild(bgAndGrad)
+
+let imageBackgroundImg = document.createElement('img')
+imageBackgroundImg.style.minWidth = '100vw'
+imageBackgroundImg.src = plain
+bgAndGrad.appendChild(imageBackgroundImg)
+
+let bgGrad = document.createElement('div')
+bgGrad.style.position = 'absolute'
+bgGrad.style.height = '50px'
+bgGrad.style.bottom = '0'
+bgGrad.style.width = '100%'
+bgGrad.style.background = 'linear-gradient(to bottom, transparent 0%, black 90%)'
+bgAndGrad.appendChild(bgGrad)
+
+let applyUnitsStyle = (units: HTMLDivElement) => {
+  units.style.display = 'grid'
+  units.style.rowGap = '2px'
+  units.style.gridTemplateColumns = 'repeat(auto-fit, clamp(120px, 50%, 200px))'
+  units.style.justifyContent = 'center'
+  units.style.zIndex = '1'
+}
+let units1 = document.createElement('div')
+applyUnitsStyle(units1)
+visual.appendChild(units1)
+
+let allies: {}[] = [{ displayName: 'guy1', sprite: bowman }, { displayName: 'guy2', sprite: bowman }]
+let vasesToShow: {}[] = [{ displayName: 'Arthur', sprite: general }]
+
+function createUnitHolder(): { unitHolder: HTMLElement, heroSprite: HTMLElement } {
+  let unitHolder = document.createElement('div')
+
+  let homePlaceholder = document.createElement('div')
+  homePlaceholder.style.order = '1'
+  homePlaceholder.style.border = '2px dashed transparent'
+  homePlaceholder.style.borderRadius = '10px'
+  homePlaceholder.style.width = '50%'
+  homePlaceholder.style.height = 'auto'
+  unitHolder.appendChild(homePlaceholder)
+
+  let visualUnitTop = document.createElement('div')
+  visualUnitTop.style.transition = 'opacity 0.5s ease-in-out'
+  homePlaceholder.appendChild(visualUnitTop)
+
+  let nameHolder = document.createElement('div')
+  nameHolder.style.display = 'flex'
+  nameHolder.style.alignItems = 'center'
+  nameHolder.style.justifyContent = 'center'
+  visualUnitTop.appendChild(nameHolder)
+
+  let nameTag = document.createElement('span')
+  nameTag.textContent = 'cool guy'
+  nameHolder.appendChild(nameTag)
+
+  let outerHeroSprite = document.createElement('div')
+  outerHeroSprite.style.position = 'relative'
+  outerHeroSprite.style.width = '100%'
+  visualUnitTop.appendChild(outerHeroSprite)
+
+  let heroSprite = document.createElement('img')
+  heroSprite.style.display = 'block'
+  heroSprite.style.width = '100%'
+  heroSprite.style.aspectRatio = '1/1'
+  heroSprite.src = bowman
+  outerHeroSprite.appendChild(heroSprite)
+  return { unitHolder: unitHolder, heroSprite: heroSprite }
+}
+
+for (let ally of allies) {
+  let unitHolder = createUnitHolder()
+  units1.appendChild(unitHolder.unitHolder)
+}
+
+
+let centerPlaceHolder = document.createElement('div')
+centerPlaceHolder.style.height = 'clamp(25px, 5vw + 1px, 50px)'
+centerPlaceHolder.style.width = 'clamp(25px, 5vw + 1px, 50px)'
+centerPlaceHolder.style.position = 'absolute'
+centerPlaceHolder.style.top = '50%'
+centerPlaceHolder.style.left = '50%'
+centerPlaceHolder.style.backgroundColor = 'blue'
+visual.appendChild(centerPlaceHolder)
+
+let units2 = document.createElement('div')
+applyUnitsStyle(units2)
+visual.appendChild(units2)
+for (let vasToShow of vasesToShow) {
+  let unitHolder = createUnitHolder()
+  unitHolder.heroSprite.style.transform = 'scaleX(-1)'
+  units2.appendChild(unitHolder.unitHolder)
+}
 
 
